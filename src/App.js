0219ce86cf1b50af41bedb1737e15881899f8125
JASIM16027 <jasimcse27@gmail.com>
2024-11-22 21:50:44 +0600

    restructure the react-app

---
+++ src/App.js
@@ -0,0 +1,6 @@
+// 4) Create a new component called App
+function App() {
+
+    return <input spellCheck={true} />
+}
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,17 +2,13 @@
 
 import React from "react"; // Library that defines what a component is and how multiple components work together
 import ReactDOM from "react-dom/client"; // Library that knows how to get a component to show up in the browser
-
+import App from './App';
 // 2) Get references to the div with root ID root
 const element = document.getElementById('root');
 
 // 3) Tell React to take control of the root element
 const root = ReactDOM.createRoot(element);
 
-// 4) Create a new component called App
-function App() {
-    return <h1>Hi, There</h1>
-}
 
 
 // 5) Render the App component to the root element
